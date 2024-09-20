e4508cd467e60ba6255fd39987e5057a7c82c615
RilzSnep <fff.fff.fff.08.fff@mail.ru>
2024-09-20 10:03:04 +0300

    apdate_3

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ class Product:
 
     def __init__(self, name, description, price, quantity):
         self.name = name
-        self.price = price
         self.description = description
+        self.price = price
         self.quantity = quantity
 
 
