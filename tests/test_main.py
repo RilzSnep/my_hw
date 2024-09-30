46a94d18fa6599b16c4223a03874dba04d5e845e
RilzSnep <fff.fff.fff.08.fff@mail.ru>
2024-09-30 15:23:38 +0300

    15.1 try 2

--- tests/test_main.py
+++ tests/test_main.py
@@ -58,17 +58,30 @@ def test_product_3(product_3):
 
 @pytest.fixture()
 def test_category() -> Any:
-    product4 = Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)
     category1 = Category(
         "Телевизоры",
-        "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником",
+        "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником"
     )
     return category1
 
 
 def test_category_init(test_category: Any) -> Any:
     assert (
-        test_category.description
-        == "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником"
+            test_category.description
+            == "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником"
     )
     assert test_category.name == "Телевизоры"
+
+
+def test_category_str():
+    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+    category1 = Category(
+        "Смартфоны",
+        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+        [product1, product2, product3])
+    assert str(category1.products) == "['Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт.', 'Iphone 15, 210000.0 руб. Остаток: 8 шт.', 'Xiaomi Redmi Note 11, 31000.0 руб. Остаток: 14 шт.']"
+    assert product1 + product2 == 2580000.0
+    assert product1 + product3 == 1334000.0
+    assert product2 + product3 == 2114000.0
